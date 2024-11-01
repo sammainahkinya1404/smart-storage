3240474fd831d5e7c81862bfa60e23c4d6cf8633
Samson Kinyanjui <sammainah98@gmail.com>
2024-11-01 04:57:04 +0300

    added Login and register components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import AddItem from './components/pages/AddItem';
+import Register from './components/pages/Register';
+import Login from './components/pages/Login';
 import './App.css';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add-item" element={<AddItem />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>

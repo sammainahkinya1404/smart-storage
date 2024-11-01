2a0e6dcf960ae6ea1f5efb5b7bdd4efb4bc914b4
Samson Kinyanjui <sammainah98@gmail.com>
2024-11-01 05:04:38 +0300

    ....

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './components/pages/Home';
 import AddItem from './components/pages/AddItem';
 import Register from './components/pages/Register';
@@ -8,12 +8,14 @@ import Login from './components/pages/Login';
 import './App.css';
 
 function App() {
+  const isAuthenticated = localStorage.getItem('authenticated') === 'true';
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add-item" element={<AddItem />} />
+          <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/add-item" element={isAuthenticated ? <AddItem /> : <Navigate to="/login" />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>

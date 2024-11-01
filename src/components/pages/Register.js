5f4c8cf7a78f822a50f46155fa64dad4e0f466a3
Samson Kinyanjui <sammainah98@gmail.com>
2024-11-01 05:11:36 +0300

    Initial commits

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -19,10 +19,25 @@ const Register = () => {
     <div>
       <h2>Register</h2>
       <form onSubmit={handleRegister}>
-        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <input 
+          type="text" 
+          placeholder="Username" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} 
+          required 
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          required 
+        />
         <button type="submit">Register</button>
       </form>
+      <p>
+        Already have an account? <a href="/login">Log in here</a>
+      </p>
     </div>
   );
 };
